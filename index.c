c0e4cc22c0b6c0fdd4a48a4d1365fc95a334777e
Laian <braumlaian@gmail.com>
2020-11-21 22:52:18 -0300

    Ponteiro de pacientes implantado, agora conseguimos ler e armazenar todos

--- index.c
+++ index.c
@@ -4,129 +4,135 @@
 
 typedef struct
 {
-    char dataCadastro[10];
-    char dataObito[10];
-    char classificacao[12];
-    char municipio[31];
-    char idadeNaDataNotificacao[31];
-    char comorbidadePulmao[3];
-    char comorbidadeRenal[3];
-    char comorbidadeDiabetes[3];
-    char comorbidadeTabagismo[3];
-    char comorbidadeObesidade[3];
-    char ficouInternado[13];
+  char dataCadastro[11];
+  char dataObito[11];
+  char classificacao[13];
+  char municipio[32];
+  char idadeNaDataNotificacao[32];
+  char comorbidadePulmao[5];
+  char comorbidadeCardio[5];
+  char comorbidadeRenal[5];
+  char comorbidadeDiabetes[5];
+  char comorbidadeTabagismo[5];
+  char comorbidadeObesidade[5];
+  char ficouInternado[16];
 } tPaciente;
 
 tPaciente LerPaciente(FILE *dados);
 
 int main()
 {
-    FILE *dados;
-    tPaciente paciente;
-    dados = fopen("covid19ES.csv", "r");
+  FILE *dados;
+  tPaciente *paciente;
+  paciente = malloc(202362 * sizeof(tPaciente));
 
-    if(dados == NULL)
-    {
-        printf("DEU MERDA");
-        exit(1);
-    }
+  char cabecalho[201];
 
-    paciente = LerPaciente(dados);
-    for(int i = 0; i < 10; i++){
-      printf("%c", paciente.dataCadastro[i]);
-    }
-    printf("\n");
-    for(int i = 0; i < 10; i++){
-      printf("%c", paciente.dataObito[i]);
-    }
-    printf("\n");
-    int tamanho = strlen(paciente.classificacao);
-    printf("----%d\n", tamanho);
-    for(int i = 0; i < tamanho; i++){
-      printf("%c", paciente.classificacao[i]);
-    }
-    printf("\n");
-    fclose(dados);
-    return 0;
+  dados = fopen("covid19ES.csv", "r");
+  if(dados == NULL)
+  {
+    printf("DEU MERDA");
+    exit(1);
+  }
+
+  fgets(cabecalho, 201, dados);
+  printf("%s", cabecalho);
+
+  for(int  i = 0; i < 202362; i++){
+    paciente[i] = LerPaciente(dados);
+  }
+
+  fclose(dados);
+  free(paciente);
+  return 0;
 }
 
 tPaciente LerPaciente(FILE *dados)
 {
+
   tPaciente paciente;
-  char charDescartavel;
-  int i;
-  for(int i = 0; i < 10; i++)
-  {
-    paciente.dataCadastro[i] = fgetc(dados);
-  }
-  charDescartavel = fgetc(dados);
+  char dadosPaciente[151];
+  unsigned int contador_virgulas = 0, indice = 0, contador_aspas = 0, i = 0;
+  fgets(dadosPaciente, 150, dados);
 
-  for(int i = 0; i < 10; i++)
-  {
-    paciente.dataObito[i] = fgetc(dados);
-  }
-  charDescartavel = fgetc(dados);
-
-  for(int i = 0; i < 30; i++)
-  {
-    charDescartavel = fgetc(dados);
-    if(charDescartavel != ',')
-    {
-      paciente.classificacao[i] = charDescartavel;
+  while(dadosPaciente[i] != '\n'){
+    if (dadosPaciente[i] == ',' && contador_aspas == 0){
+      contador_virgulas++;
+      indice = 0;
+      i++;
+      continue;
     }
-    else
-    {
-      break;
+
+    if(dadosPaciente[i] == '"'){
+      if(contador_aspas == 0){
+        contador_aspas++;
+      }else{
+        contador_aspas = 0;
+      }
     }
-  }
-  for(int i = 0; i < 40; i++)
-  {
-    charDescartavel = fgetc(dados);
-    if(charDescartavel != ',')
-    {
-      paciente.municipio[i] = charDescartavel;
+
+    if(contador_virgulas == 0){
+      paciente.dataCadastro[indice] = dadosPaciente[i];
+      paciente.dataCadastro[indice+1] = '\0';
     }
-    else
-    {
-      break;
+    else if(contador_virgulas == 1){
+      paciente.dataObito[indice] = dadosPaciente[i];
+      paciente.dataObito[indice+1] = '\0';
     }
+    else if(contador_virgulas == 2){
+      paciente.classificacao[indice] = dadosPaciente[i];
+      paciente.classificacao[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 3){
+      paciente.municipio[indice] = dadosPaciente[i];
+      paciente.municipio[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 4){
+      paciente.idadeNaDataNotificacao[indice] = dadosPaciente[i];
+      paciente.idadeNaDataNotificacao[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 5){
+      paciente.comorbidadePulmao[indice] = dadosPaciente[i];
+      paciente.comorbidadePulmao[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 6){
+      paciente.comorbidadeCardio[indice] = dadosPaciente[i];
+      paciente.comorbidadeCardio[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 7){
+      paciente.comorbidadeRenal[indice] = dadosPaciente[i];
+      paciente.comorbidadeRenal[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 8){
+      paciente.comorbidadeDiabetes[indice] = dadosPaciente[i];
+      paciente.comorbidadeDiabetes[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 9){
+      paciente.comorbidadeTabagismo[indice] = dadosPaciente[i];
+      paciente.comorbidadeTabagismo[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 10){
+      paciente.comorbidadeObesidade[indice] = dadosPaciente[i];
+      paciente.comorbidadeObesidade[indice+1] = '\0';
+    }
+    else if(contador_virgulas == 11){
+      paciente.ficouInternado[indice] = dadosPaciente[i];
+      paciente.ficouInternado[indice+1] = '\0';
+    }
+    i++;
+    indice++;
   }
 
-  for(int i = 0; i < 3; i++)
-  {
-    paciente.comorbidadePulmao[i] = fgetc(dados);
-  }
-  
-  charDescartavel = fgetc(dados);
-  for(int i = 0; i < 3; i++)
-  {
-    paciente.comorbidadeRenal[i] = fgetc(dados);
-  }
-
-  charDescartavel = fgetc(dados);
-  for(int i = 0; i < 3; i++)
-  {
-    paciente.comorbidadeDiabetes[i] = fgetc(dados);
-  }
-
-  charDescartavel = fgetc(dados);
-  for(int i = 0; i < 3; i++)
-  {
-    paciente.comorbidadeTabagismo[i] = fgetc(dados);
-  }
-
-  charDescartavel = fgetc(dados);
-  for(int i = 0; i < 3; i++)
-  {
-    paciente.comorbidadeObesidade[i] = fgetc(dados);
-  }
-
-  charDescartavel = fgetc(dados);
-  for(int i = 0; i < 3; i++)
-  {
-    paciente.ficouInternado[i] = fgetc(dados);
-  }
-
-  charDescartavel = fgetc(dados);
-  return paciente;
+  // printf("%s,", paciente.dataCadastro);
+  // printf("%s,", paciente.dataObito);
+  // printf("%s,", paciente.classificacao);
+  // printf("%s,", paciente.municipio);
+  // printf("%s,", paciente.idadeNaDataNotificacao);
+  // printf("%s,", paciente.comorbidadePulmao);
+  // printf("%s,", paciente.comorbidadeCardio);
+  // printf("%s,", paciente.comorbidadeRenal);
+  // printf("%s,", paciente.comorbidadeDiabetes);
+  // printf("%s,", paciente.comorbidadeTabagismo);
+  // printf("%s,", paciente.comorbidadeObesidade);
+  // printf("%s\n", paciente.ficouInternado);
 }
